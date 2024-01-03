7c3e51723eb4cde6d8907e8bb67304e484c1fcf2
Victória Cabral Quintério <quinterio.victoria@hotmail.com>
2024-01-02 23:15:11 -0300

    adicionado o criar e exibir os projetos

---
+++ src/components/layout/Footer.js
@@ -0,0 +1,24 @@
+import styles from './Footer.module.css';
+import {FaFacebook, FaInstagram, FaLinkedin} from 'react-icons/fa';
+
+function Footer(props) {
+    return (
+        <footer className={styles.footer}>
+            <ul className={styles.social_list}>
+                <li className={styles.social_list}>
+                    <FaFacebook />
+                </li>
+                <li>
+                    <FaInstagram />
+                </li>
+                <li>
+                    <FaLinkedin />
+                </li>
+            </ul>
+            <p className={styles.copy_right}> <span>Budget Manager</span> &copy; 2024</p>
+        </footer>
+    );
+   
+}
+
+export default Footer;
---
+++ src/components/layout/Navbar.js
@@ -0,0 +1,35 @@
+import styles from "./Navbar.module.css";
+import { Link } from "react-router-dom";
+import Container from "./Container";
+import logo from "../../img/logo.png";
+
+function Navbar(props) {
+  return (
+    <nav className={styles.navbar}>
+      <Container>
+        <Link to="/">
+          <img src={logo} alt="budget-manager" />
+        </Link>
+        <ul className={styles.list}>
+          <li className={styles.item}>
+            <Link to="/">Home</Link>
+          </li>
+          <li className={styles.item}>
+            <Link to="/projects">Projetos</Link>
+          </li>
+          <li className={styles.item}>
+            <Link to="/company">Empresa</Link>
+          </li>          
+          <li className={styles.item}>
+            <Link to="/newproject">Novo Projeto</Link>
+          </li>
+          <li className={styles.item}>
+            <Link to="/contact">Contato</Link>
+          </li>
+        </ul>
+      </Container>
+    </nav>
+  );
+}
+
+export default Navbar;
