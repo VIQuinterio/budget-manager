86ae1361b30595a328388ba5f66ca2bfc501b972
Victória Cabral Quintério <quinterio.victoria@hotmail.com>
2024-01-03 23:27:10 -0300

    Componente Loading e Atualizar projetos

---
+++ src/components/pages/Project.js
@@ -0,0 +1,104 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+
+import Loading from "../layout/Loading";
+import Container from "../layout/Container";
+import ProjectForm from '../project/ProjectForm';
+import Message from '../layout/Message';
+
+import styles from "./Project.module.css";
+
+function Project() {
+    const { id } = useParams();
+    const [project, setProject] = useState([]);
+    const [showProjectForm, setshowProjectForm] = useState(false);
+    const [message, setMessage] = useState();
+    const [type, setType] = useState();
+
+    useEffect(() => {
+        setTimeout(() => {
+            fetch(`http://localhost:5000/projects/${id}`, {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            })
+            .then((resp) => resp.json())
+            .then((data) => {
+                setProject(data);            
+            })
+            .catch((error) => console.log(error));
+        }, 300)
+    }, [id]);
+
+    function editPost(project) {
+        if (project.budget < project.cost) {
+            setMessage('O orçamento não pode ser menor que o custo do projeto!');
+            setType('error');
+            return false
+        }
+        /*O PATCH SÓ ALTERA O QUE FOR DIREFENTE NO BANCO*/
+        fetch(`http://localhost:5000/projects/${project.id}`, {
+            method: 'PATCH',
+            headers:{
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(project),
+        })
+        .then((resp) => resp.json())
+        .then((data) =>{
+            setProject(data);
+            setshowProjectForm(false);
+            setMessage('Projeto atualizado com sucesso!');
+            setType('success');
+        })
+        .catch((err) => console.log(err));
+    }
+
+    function toggleProjectForm() {
+        setshowProjectForm(!showProjectForm);
+    }
+
+    return (
+        <div>            
+            {project.name ? (                
+                <div className={styles.project_details}>                    
+                    <Container customClass="column">
+                    {message && <Message type={type} msg={message} />}
+                        <div className={styles.details_container}>
+                            <h1 className={styles.title_details}>Projeto: {project.name}</h1>
+                            <button className={styles.btn} onClick={toggleProjectForm}>
+                                {!showProjectForm ? 'Editar projeto' : 'Fechar'}
+                            </button>
+                            {!showProjectForm ? (
+                                <div className={styles.project_info}>
+                                    <p>
+                                        <span>Categoria:</span>{project.category.name}
+                                    </p>
+                                    <p>
+                                        <span>Total de Orçamento:</span> R${project.budget}
+                                    </p>
+                                    <p>
+                                        <span>Total Utilizado::</span> R${project.cost}
+                                    </p>
+                                </div>
+                            ) : (
+                                <div className={styles.project_info}>
+                                    <ProjectForm 
+                                        handleSubmit={editPost}
+                                        btnText="Concluir edição"
+                                        projectData={project}
+                                    />
+                                </div>
+                            )}
+                        </div>                        
+                    </Container>
+                </div>
+            ) : (
+                <Loading />
+            )}
+        </div>        
+    );
+}
+
+export default Project;
